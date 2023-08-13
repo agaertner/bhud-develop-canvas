0e67e332415e0a29b273895c08350e9870d4de22
Andreas G <13819164+agaertner@users.noreply.github.com>
2023-08-13 15:53:36 +0200

    feat: add proj

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace canvas
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var formSize = new Size(1280, 720); // Default form size
+            var backColor = Color.FromArgb(255,0,255,0); // Default background color
+
+            for (int i = 0; i < args.Length; i += 2)
+            {
+                if (i + 1 < args.Length)
+                {
+                    if (args[i] == "--resolution")
+                    {
+                        string[] resolutionArgs = args[i + 1].Split('x');
+                        if (resolutionArgs.Length == 2 && int.TryParse(resolutionArgs[0], out int width) && int.TryParse(resolutionArgs[1], out int height))
+                        {
+                            formSize = new Size(width, height);
+                        }
+                    }
+                    else if (args[i] == "--color")
+                    {
+                        try
+                        {
+                            backColor = ColorTranslator.FromHtml(args[i + 1]);
+                        }
+                        catch (Exception)
+                        {
+                            // Handle invalid color format if needed
+                        }
+                    }
+                }
+            }
+            Application.Run(new Canvas(formSize, backColor));
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Blish HUD - Module Development Canvas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Blish HUD - Module Development Canvas")]
+[assembly: AssemblyCopyright("Copyright © A. Gärtner 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c953247c-04b8-4d13-94a2-2ae2ca97e0b4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
